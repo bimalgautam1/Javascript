47845c67c716a24ead99bad242901f5545fae069
bimal69 <gbimal3210@gmail.com>
2024-06-11 20:50:07 +0545

    Updated

---
+++ index.js
@@ -0,0 +1,32 @@
+// var arr=[1,2,4,5,6,7,8,9,10,11,12,13,14,15];
+// arr.forEach(function(val){
+//     console.log(val+2);
+// })
+
+// const arr2 = arr.map(function(val){
+//     return val+1; 
+// });
+// console.log(arr2);
+
+// const arr3 = arr.filter(function(value){
+//     if(value>8){return true;}
+//     else {return false};
+// })
+// console.log(arr3);
+
+// const arr4 = arr.find(function(value){
+//     if(value==5){
+//         return value;
+//     }
+// })
+// console.log(arr4);
+
+// arr.indexOf(8);
+
+// async function abcd(){
+//     var bolb = await fetch(`https://randomuser.me/api/`);
+//     var ans = await bolb.json();
+//     console.log(ans.results);
+//     console.log("Hello There");
+// }
+// abcd();
